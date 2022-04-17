c5a5f01b53180c474a28a7c805148c81fe6f49cb
moziauddin <38300517+moziauddin@users.noreply.github.com>
2022-04-17 14:40:45 +1000

    Create delete-snapshots-by-filter.sh

---
+++ cloud/aws/delete-snapshots-by-filter.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Dependencies: 
+# Install jq
+# Assign permissions to the instance running the script
+#   1. Using IAM role (preferred)
+#   2. Using AWS Key and Shared Secret in .aws/.credentials
+
+if [[ -z $1 && -z $2 ]]; then
+	echo ""
+	echo " ERROR: Please pass the filter attribute as first argument and its value as second with wild card"
+	echo "    Example: ./delete-old-snapshots.sh 'start-time' '2016*'"
+    echo "    to delete all snapshots matching deleteOn date of Jan 2021"
+	echo ""
+	exit 1
+fi
+echo "Proceed with caution!"
+echo "Once deleted, snapshots cannot be recovered"
+snap_ids=$(aws ec2 describe-snapshots --filters Name=$1,Values=$2 | jq .Snapshots[].SnapshotId)
+matched=$(echo $snap_ids | wc -w)
+echo "Matched Snapshots: "$matched
+
+if [[ $matched -ne 0 ]]; then
+    for id in $snap_ids; 
+    do
+        new_id=$(echo $id | sed -e s/\"//g)
+        echo "deleteing $new_id"
+        # aws ec2 delete-snapshot --snapshot-id $new_id
+    done
+else
+    echo "No Snapshots to delete"
+fi
